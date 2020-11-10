cd25dcd6f3734fa8120e066b761adc7f39f4e100
iramosromero <ramos.israel916@gmail.com>
2020-11-09 22:24:53 -0800

    Adding database and connection file

---
+++ database/DB.py
@@ -0,0 +1,11 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+dblist = myclient.list_database_names()
+
+if "mydatabase" in dblist:
+      print("The database exists.")
+#mydb = myclient["TweetyBird"]
+
+
