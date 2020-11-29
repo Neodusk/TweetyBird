8da9b85f40211ece6c68a588fb67f998427d338c
Neodusk <htortorich@mail.csuchico.edu>
2020-11-28 21:50:02 -0800

    Update user_motion_code.py

--- user_motion_code.py
+++ user_motion_code.py
@@ -44,8 +44,8 @@ def userMotionCode(filenamePath):
     access_token = ‘XXX’
 
     # authorisation process, using the keys and tokens
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(consumer_key)
+    auth.set_access_token(access_token)
     # creation of the actual interface, using authentication
     api = tweepy.API(auth)
 
