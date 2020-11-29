c2efe7941eee094ca7096b6ea9bae750220113bc
Neodusk <htortorich@mail.csuchico.edu>
2020-11-28 21:47:59 -0800

    Update user_motion_code.py

--- user_motion_code.py
+++ user_motion_code.py
@@ -21,40 +21,27 @@ For more information see pi-timolo github Wiki
 
 #------------------------------------------------------------------------------
 def userMotionCode(filenamePath):
-    # we need to create an instance of the Google Vision API
     client = storage.Client()
-    # instantiates a client
     client = vision.ImageAnnotatorClient()
 
-    # loads the image into memory
     with io.open(filename, 'rb') as image_file:
         content = image_file.read()
 
     image = types.Image(content=content)
 
-    # performs label detection on the image file
     response = client.label_detection(image=image)
-    # pass the response into a variable
     labels = response.label_annotations
 
-    # we have our labels, now create a string to add to the tweet message
-    # for debugging - let’s see what Google thinks is in the image
     print('Labels:')
-    # add labels to our tweet text
     tweetText = "Labels: "
     animalInPic = False
     for label in labels:
         print(label.description)
         tweetText = tweetText + " " + label.description
-        # edit this line to change the animal you want to detect
         if "bird" in tweetText: animalInPic = true
-
-    # set up Tweepy
-    # consumer keys and access tokens, used for authorisation
+    
     consumer_key = ‘XXX’
-    consumer_secret = ‘XXX’
     access_token = ‘XXX’
-    access_token_secret = ‘XXX’
 
     # authorisation process, using the keys and tokens
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
